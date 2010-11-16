9d0747e725face143391007b5b1e36df9336997e
Kenny Ortmann <kenny.ortmann@gmail.com>
2010-11-16 13:19:33 -0600

    changing test output to make sense

--- spec/models/foo_spec.rb
+++ spec/models/foo_spec.rb
@@ -8,17 +8,14 @@ describe Foo do
       foo_mock.stub(:name){'foo mock'}
       Foo.should_receive(:new){foo_mock}
       puts "foo_mock --#{Foo.new.name}--"
-      bar_mock = mock
-      bar_mock.stub(:name){'bar mock'}
-      Bar.should_receive(:new){bar_mock}
-      puts "bar_mock --#{Bar.new.name}"
+      @bar_mock = mock
+      @bar_mock.stub(:name){'bar mock'}
+      Bar.should_receive(:new){@bar_mock}
+      Bar.new.name
     end
     it { true.should == true }
   end
   describe 'situation 2' do
-    before do
-      puts "after --#{Bar.new.name}--"    
-    end
-    it { true.should == true }
+    it { Bar.new.name.should == 'barish' }
   end
 end
